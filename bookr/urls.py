6e5963736644074212288b4be28c02bea2707081
Jansen <andre@jansenbolders.nl>
2023-08-23 14:37:30 +0200

    second commit

--- bookr/urls.py
+++ bookr/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from reviews import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index),
+    path('book-search', views.book_search),
 ]
