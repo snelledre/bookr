72f7425a9e4a70e92aed73be660a5de1135e985f
Jansen <andre@jansenbolders.nl>
2023-08-24 14:46:09 +0200

    admin area

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.admin import AdminSite
+from reviews.models import Publisher, Contributor, Book, BookContributor, Review
 
-# Register your models here.
+class BookrAdminSite(admin.AdminSite):
+    title_header = 'Bookr Admin'
+    site_header = 'Bookr administration'
+    index_title = 'Bookr site admin'
+
--- reviews/models.py
+++ reviews/models.py
@@ -8,6 +8,9 @@ class Publisher(models.Model):
     website = models.URLField(help_text="The Publisher's website.")
     email = models.EmailField(help_text="The Publisher's email address.")
 
+    def __str__(self):
+        return self.name
+
 
 class Book(models.Model):
     """A published book."""
@@ -24,6 +27,18 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Contributor(models.Model):
+    """A contributor to a Book, e.g. author, editor, co-author."""
+    first_names = models.CharField(max_length=50,
+                                   help_text="The contributor's first name or names.")
+    last_names = models.CharField(max_length=50,
+                                  help_text="The contributor's last name or names.")
+    email = models.EmailField(help_text="The contact email for the contributor.")
+
+    def __str__(self):
+        return self.first_names
 
 
 class BookContributor(models.Model):
